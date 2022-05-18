69cffc541da89ae46a5a7af13a76d7772a76f48b
chokri.bs <chokri.bs@gmail.com>
2022-05-18 17:06:29 +0100

    LEMANS:Billetterie: ajout model

---
+++ src/main/java/fr/symphonie/tools/lemans/bt/model/SpectacleDetails.java
@@ -0,0 +1,80 @@
+package fr.symphonie.tools.lemans.bt.model;
+
+import fr.symphonie.util.model.SimpleEntity;
+import fr.symphonie.util.model.Trace;
+import lombok.Data;
+@Data
+public class SpectacleDetails {
+	private Integer exercice;
+	/**
+	 * Code Spectacle
+	 */
+	private String code;
+	/**
+	 * Le taux TVA
+	 */
+	private float tva;
+	/**
+	 * Adresse budgétaire
+	 */
+	private SimpleEntity destination;
+	private SimpleEntity nature;
+	private SimpleEntity service;
+	private SimpleEntity programme;
+	/**
+	 * Compte de produit
+	 */
+	private SimpleEntity compteProduit;
+	private String imputTtc;
+	private String imputTva;
+	private Trace trace;
+	private Spectacle spectacle;
+	private Integer noLbi;
+	
+	
+	public void setArticle(Spectacle spectacle) {
+		this.spectacle = spectacle;
+		if(spectacle!=null)setCode(spectacle.getCode());
+	}
+	
+	public boolean isExecNull() {
+		return (getExercice()==null)||
+	            (getExercice().intValue()==0); 
+	             
+	}
+	public boolean isCompteProduitNull() {
+		return (getCompteProduit().getCode()==null)||
+	            (getCompteProduit().getCode().trim().isEmpty()); 
+	             
+	}
+	public boolean isDestinationNull() {
+		return (getDestination().getCode()==null)||
+	            (getDestination().getCode().trim().isEmpty()); 
+	             
+	}
+	public boolean isProgrammeNull() {
+		return (getProgramme().getCode()==null)||
+	            (getProgramme().getCode().trim().isEmpty()); 
+	             
+	}
+	public boolean isNatureNull() {
+		return (getNature().getCode()==null)||
+	            (getNature().getCode().trim().isEmpty()); 
+	             
+	}
+	public boolean isServiceNull() {
+		return (getService().getCode()==null)||
+	            (getService().getCode().trim().isEmpty()); 
+	             
+	}
+	
+	public boolean checkRequired() {
+		if(isCompteProduitNull())return false;
+		if(isDestinationNull())return false;
+		if(isExecNull())return false;
+		if(isNatureNull())return false;
+		if(isProgrammeNull())return false;
+		if(isServiceNull())return false;
+		return true;
+	}
+}
